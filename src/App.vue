d033301ba7e26f6a342429d4d8b3b8e0043c8c12
Alex Pacheco <alex.pacheco.s@gmail.com>
2017-09-27 02:19:48 -0300

    Configurando la API de Lastfm en Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>VueMusic</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="artist in artists">{{ artist.name }}</li>
     </ul>
   </div>
 </template>
 
 <script>
+import getArtists from './api'
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      artists: []
     }
+  },
+  mounted: function () {
+    const self = this
+    getArtists()
+      .then(function (artists) {
+        self.artists = artists
+      })
   }
 }
 </script>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: 'vuemusic',
+  apiKey: '69f5f1418d8aad84b3e240b9c6f35397',
+  secret: 'e728ff1ff884f9afd992d51191973e6f',
+  RegisteredTo: 'almapase'
+}
