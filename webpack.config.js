cb898640ee6c4ef270441e748528c4ea93e005d5
Alex Pacheco <alex.pacheco.s@gmail.com>
2017-09-27 14:52:47 -0300

    fix publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/VueMusic/dist/',
     filename: 'build.js'
   },
   module: {
